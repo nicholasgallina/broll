09f12bed30fb5dfaae9f3042ed276286772a5784
Nicholas Gallina <nicholasgallina@Aurora.local>
2025-11-19 18:06:03 -0500

    header styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,14 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
         brand: "#006917",
-        testcolor: "ff0000",
-      }
+        testcolor: "#ff0000",
+      },
     },
   },
   plugins: [],
